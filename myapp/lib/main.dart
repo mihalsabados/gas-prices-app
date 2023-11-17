a718c651e159374b3e9847d7ef115fc65aadcb66
mihalsabados <sabadosmihal00@gmail.com>
2023-11-17 19:01:39 +0100

    Started with layout and added containers with static prices

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -13,25 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Gas Prices',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Gas Prices Home Page'),
+      home: const MyHomePage(title: 'Gas Prices'),
     );
   }
 }
@@ -59,67 +44,158 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            // Spacer(),
+            SizedBox(height: 20),
+            Text('Currency'),
+            SingleChoice(),
+            SizedBox(height: 20),
+            // Spacer(),
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              width: 400,
+              height: 100.0,
+              decoration: dieselBoxDecoration(),
+              child: Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Diesel", style: TextStyle(color: Colors.white)),
+                  Row(
+                    children: [
+                      Text("204",
+                          style: TextStyle(fontSize: 30, color: Colors.white)),
+                      SizedBox(width: 2),
+                      Text(
+                        "rsd",
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      ),
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  )
+                ],
+              )),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              width: 400,
+              height: 100.0,
+              decoration: petrolBoxDecoration(),
+              child: Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Petrol"),
+                  Row(
+                    children: [
+                      Text("180", style: TextStyle(fontSize: 30)),
+                      SizedBox(width: 2),
+                      Text("rsd", style: TextStyle(fontSize: 14)),
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  )
+                ],
+              )),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              width: 400,
+              height: 100.0,
+              decoration: lpgBoxDecoration(),
+              child: Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("LPG"),
+                  Row(
+                    children: [
+                      Text("93.4", style: TextStyle(fontSize: 30)),
+                      SizedBox(width: 2),
+                      Text("rsd", style: TextStyle(fontSize: 14)),
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  )
+                ],
+              )),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+enum Currency { rsd, eur, usd }
+
+class SingleChoice extends StatefulWidget {
+  const SingleChoice({super.key});
+
+  @override
+  State<SingleChoice> createState() => _SingleChoiceState();
+}
+
+class _SingleChoiceState extends State<SingleChoice> {
+  Currency currencyView = Currency.rsd;
+
+  @override
+  Widget build(BuildContext context) {
+    return SegmentedButton<Currency>(
+      segments: const <ButtonSegment<Currency>>[
+        ButtonSegment<Currency>(
+            value: Currency.rsd, label: Text('RSD'), icon: Icon(Icons.money)),
+        ButtonSegment<Currency>(
+            value: Currency.eur, label: Text('EUR'), icon: Icon(Icons.euro)),
+        ButtonSegment<Currency>(
+            value: Currency.usd,
+            label: Text('USD'),
+            icon: Icon(Icons.attach_money)),
+      ],
+      selected: <Currency>{currencyView},
+      onSelectionChanged: (Set<Currency> newSelection) {
+        setState(() {
+          currencyView = newSelection.first;
+        });
+      },
+    );
+  }
+}
+
+BoxDecoration dieselBoxDecoration() {
+  return BoxDecoration(
+    border: Border.all(width: 2.0, color: Colors.white),
+    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+    color: Colors.black,
+  );
+}
+
+BoxDecoration petrolBoxDecoration() {
+  return BoxDecoration(
+    border: Border.all(width: 2.0),
+    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+    color: Colors.green,
+  );
+}
+
+BoxDecoration lpgBoxDecoration() {
+  return BoxDecoration(
+    border: Border.all(width: 2.0),
+    borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+    // color: Colors.red,
+  );
 }
